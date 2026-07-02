4638f9fa994a5928ecada1a836b15469014104f2
gl0008 <glorialiu0509@gmail.com>
2026-07-02 00:08:13 -0400

    Update config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+window.TIMEBRIDGE_CONFIG = {
+  supabaseUrl: "https://qsnbhyfmsfcgpqkwipbq.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzbmJoeWZtc2ZjZ3Bxa3dpcGJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5NDk4MjgsImV4cCI6MjA5ODUyNTgyOH0.Ag-Kv6LP3BQcFENEakSfBid84NC3XhUhC28QowNoIqI",
+  scheduleId: "weekend",
+};
+document.documentElement.dataset.timebridgeConfig = "loaded";
